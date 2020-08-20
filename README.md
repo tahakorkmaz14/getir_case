9181c0875e593f192b382c27cdc39851dcfe725f
EXT02D22747 <taha.korkmaz@consultant.turkcell.com.tr>
2020-08-20 10:57:34 +0300

    Added changes for heroku

--- README.md
+++ README.md
@@ -1,26 +1,128 @@
-# getir_case
+# Getir Case Study
 
-A simple Node.js Express api that handles the post requests
+> NodeJS application for Getir case study
 
-## Request Example
+---
 
-Send your requests to the /getir_test/records with a Json Body like this:
-``
-{ 
-  "startDate": "2016-01-26", 
-  "endDate": "2018-02-02", 
-  "minCount": 2100, 
-  "maxCount": 2100 
-}
-``
-Response will be like this:
-``
+## Table of Contents
+
+- [Installation](#installation)
+- [Example](#example)
+- [Tests](#tests)
+- [Technologies](#technologies)
+
+## Installation
+
+### Clone
+
+- Clone this repo to your local machine using `https://github.com/apasabeyoglu/getir`
+
+### Setup
+
+- If you want more syntax highlighting, format your code like this:
+
+> update and install these packages first
+
+```shell
+$ brew update
+$ npm install
+```
+
+> now install and start mongodb
+
+```shell
+$ brew tap mongodb/brew
+$ brew install mongodb-community@4.2
+$ brew services start mongodb-community@4.2
+```
+
+> setup mongodb for projects
+
+```shell
+$ npm install -g mongodb
+```
+
+> install body parser for Post requests (REST API)
+
+```shell
+$ npm install --save body-parser
+```
+
+> install Jest for testing
+
+```shell
+$ npm install --save-dev jest supertest
+```
+
+> install Heroku for deployment
+
+```shell
+$ brew tap heroku/brew
+$ brew install heroku/brew
+```
+
+---
+
+## Example
+
+> Deployed Heroku application link
+  
+  https://vibrant-moliere-62617.herokuapp.com/get
+
+> Example Body of the POST request
+
+```
 {
-    "code": 0,
-    "msg": "Success",
-    "records": [
-        {...
-        }
-    ]
+"startDate": "2016-01-26",
+"endDate": "2018-02-02",
+"minCount": 2000,
+"maxCount": 3000
 }
-``
+```
+
+> Example response of the above request
+
+```
+{
+    "code": "0",
+    "msg": "success",
+    "records": [
+        {
+            "key": "HKNUadsw",
+            "createdAt": "2016-12-18T03:25:12.976Z",
+            "totalCount": 2364
+        },
+        {
+            "key": "bxoQiSKL",
+            "createdAt": "2016-01-29T01:59:53.494Z",
+            "totalCount": 2991
+        },
+        {
+            "key": "dcJUSDLR",
+            "createdAt": "2016-02-27T16:12:30.813Z",
+            "totalCount": 2780
+        },
+        .
+        .
+        .
+}   
+```
+
+---
+
+## Tests
+
+> Check the test results in the local directory
+
+```shell
+$ npm test
+```
+
+---
+
+## Technologies
+
+- NodeJS v12.18.2
+- MongoDB Community Edition@4.2
+- Jest and Supertest
+- Heroku
--- app.js
+++ app.js
@@ -1,19 +1,68 @@
-require('./config');
+var express = require('express');
+var createError = require('http-errors');
+const bodyParser = require("body-parser");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
 
-const Server = require('./init/Server');
-const Mongo = require('./init/Mongo');
+var app = express();
 
-class App {
-	static async initialize() {
-		console.info('GETIR CASE STUDY APP STARTING');
-		const server = new Server();
-		await server.start();
-		await Mongo.initialize();
-	}
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+class UserResponse {
+    code;
+    msg;
+    records;
 }
-App.initialize()
-    .then(() => console.log('GETIR CASE STUDY APP STARTED'))
-    .catch(err => {
-			console.error(err);
-			process.exit();
-		});
+
+app.post('/get', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("getir-case-study");
+        var projectionQ = {_id:0, key:1, createdAt:1, totalCount:{"$sum": "$counts"}};
+        var matchQ = {$and: [
+            {createdAt: { $gte: new Date (req.body.startDate) }},
+            {createdAt: { $lte: new Date(req.body.endDate) }},
+            {totalCount: {$gte:req.body.minCount}},
+            {totalCount: {$lte:req.body.maxCount}}
+         ]};
+
+        dbo.collection("records").aggregate().project(projectionQ).match(matchQ).toArray(function(err, result) {
+            if (err) throw err;
+            const response = new UserResponse();
+            response.code = "0";
+            response.msg = "success";
+            response.records = result;
+            res.json(response)
+            db.close();
+        });
+    });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    const response = new UserResponse();
+    response.code = err.status.toString();
+    response.msg = err.message;
+    response.records = [];
+    res.json(response)
+});
+
+module.exports = app;
